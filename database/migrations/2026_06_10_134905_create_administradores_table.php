87b2c827c8356ca7b4ca1f0c6933c60a87da4e92
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2026-06-24 09:35:42 -0400

    fix: administradors-> administradores 2

--- database/migrations/2026_06_10_134905_create_administradores_table.php
+++ database/migrations/2026_06_10_134905_create_administradores_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('administradors', function (Blueprint $table) {
+        Schema::create('administradores', function (Blueprint $table) {
             $table->id();
 
             $table->string('nombre');
